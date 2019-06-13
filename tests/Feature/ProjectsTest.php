12d396ce934e1a688c9948774638a32527494918
Tyler Collier <git@tylercollier.com>
2019-06-13 10:07:48 -0700

    Finish episode 3

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -23,4 +23,13 @@ class ProjectsTest extends TestCase
         $this->assertDatabaseHas('projects', $attributes);
         $this->get('/projects')->assertSee($attributes['title']);
     }
+
+    public function test_a_project_requires_a_title() {
+        $attributes = factory('App\Project')->raw(['title' => '']);
+        $this->post('/projects', $attributes)->assertSessionHasErrors('title');
+    }
+
+    public function test_a_project_requires_a_description() {
+        $this->post('/projects', [])->assertSessionHasErrors('description');
+    }
 }
